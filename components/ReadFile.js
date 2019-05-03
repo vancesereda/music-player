5acce56f88d9104c4e5d2ece5d2aa3da384905c9
Vance Sereda <vancesereda@gmail.com>
2019-05-02 21:38:32 -0700

    file read

---
+++ components/ReadFile.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Text, Stylesheet} from 'react-native';
+import TrackPlayer from 'react-native-track-player';
+
+export default class ReadFile extends Component {
+
+
+    constructor(props) {
+        super(props)
+        this.state = {  /* ... */ }
+    }
+
+
+    readFile = async () => {
+
+    }
+
+
+
+
+    render() {
+        
+        return (
+        <View>
+
+
+        </View>)
+    }
+}
